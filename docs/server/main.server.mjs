564404d0d1594c71d3d18766ba478b152c44a832
abhishekyadav11 <abhisyadav11@gmail.com>
2024-04-07 17:22:58 +0530

    Aeonaxy Web Assignment

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-43IPVB3V.mjs";import"./chunk-IBH7K5CB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2D64YLLP.mjs";import"./chunk-IBH7K5CB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};

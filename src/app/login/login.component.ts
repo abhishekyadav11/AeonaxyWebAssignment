b5e1650a323da46f751d88a8e3238af208100623
abhishekyadav11 <abhisyadav11@gmail.com>
2024-04-09 18:22:04 +0530

    Aeonaxy Web Assignment

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  constructor(private router:Router)
-  {
 
-  }
-
-  redirectToProfile()
-  {
-    this.router.navigate(['/profile']);
-  }
 }
